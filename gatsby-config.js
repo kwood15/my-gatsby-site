60acce974e24845ea106a3323ad531d33a9db405
Kay Wood <kwood15@users.noreply.github.com>
2021-06-18 23:32:43 +0100

    Setup percy post build to test actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-react-helmet", {
+      resolve: `gatsby-plugin-percy`,
+      options: {
+        // example options:
+        // files: [`dir/*.html`],
+        // ignore: [`ignore/*.html`],
+        // config: `config/.percy.yaml`,
+      }
+    }]
 };
